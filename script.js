3857692a993fc4108785abb58c1467d713d0a142
Harsh Bansal <harshbansal8705@gmail.com>
2023-04-21 09:30:41 +0530

    Added Hamburger menu animation

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 let elem = document.getElementsByClassName("hamburger")[0];
 elem.addEventListener("click", () => {
+    let menu = document.getElementsByClassName("menu")[0];
     if (elem.classList.contains("is-active"))
     {
         elem.classList.remove("is-active");
     }
     else {
         elem.classList.add("is-active");
+    }
+    if (menu.classList.contains("menu-visible"))
+    {
+        menu.classList.remove("menu-visible");
+    }
+    else {
+        menu.classList.add("menu-visible");
     }
 })
